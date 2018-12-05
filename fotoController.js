01bd3559de1cdbc079ea91ba97ee06ceb7bda2f4
Sophia <sgigliotti@westmont.edu>
2018-12-04 22:09:05 -0800

    more files added

---
+++ fotoController.js
@@ -0,0 +1,10 @@
+//object.onmouseover = function(){myScript};
+
+function enlarge(f) {
+   f.style.width = "50%";
+}
+
+
+function normalSize(f) {
+   f.style.width = "25%";
+}
---
+++ navController.js
@@ -0,0 +1,7 @@
+function darken(dot){
+  dot.style.background = "#5D586B";
+}
+
+function normal(dot){
+  dot.style.background = "#bbb";
+}
