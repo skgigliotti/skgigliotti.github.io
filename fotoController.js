b5e2f4f6c8ef2be5a1d8fca71568295c3d68f600
Sophia <sgigliotti@westmont.edu>
2018-12-09 20:57:49 -0800

    improvemnts

--- fotoController.js
+++ fotoController.js
@@ -1,10 +1,10 @@
 //object.onmouseover = function(){myScript};
 
 function enlarge(f) {
-   f.style.width = "50%";
+   //f.style.width = "50%";
 }
 
 
 function normalSize(f) {
-   f.style.width = "25%";
+  // f.style.width = "25%";
 }
