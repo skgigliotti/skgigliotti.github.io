977421d0e32748fc2c2f0e64459387485fec04d1
Sophia <sgigliotti@westmont.edu>
2018-12-17 13:58:35 -0800

    able to display fotos

--- fotoController.js
+++ fotoController.js
@@ -1,13 +1,25 @@
 //object.onmouseover = function(){myScript};
 
-function enlarge(f) {
-   //f.style.width = "50%";
-}
+function enlarge(foto,display,toDisplay,capt) {
+   // Get the display
+   var dis = display;
 
+   // Get the image for the display - use its "alt" text as a caption
+   var img = foto;
 
-function normalSize(f) {
-  // f.style.width = "25%";
+   var diplayImg = toDisplay;
+   var captionText = capt;
+   img.onclick = function(){
+
+     dis.style.display = "block";
+     diplayImg.src = this.src;
+     captionText.innerHTML = this.alt;
+   }
+   // Get the <span> element that closes the modal
+   var xOut = document.getElementsByClassName("close")[0];
+   
+   // When the user clicks on <span> (x), close the modal
+   xOut.onclick = function() {
+     dis.style.display = "none";
+   }
 }
-$( function() {
-    $( "#dialog" ).dialog();
-  } );
