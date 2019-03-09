229fcabd04cb8bf7029a09daab49ec71555f1277
Sophia <sgigliotti@westmont.edu>
2019-03-09 12:44:03 -0800

    updating photo

--- fotoController.js
+++ fotoController.js
@@ -7,7 +7,7 @@ function enlarge(foto,display,toDisplay,capt) {
    // Get the image for the display - use its "alt" text as a caption
    var img = foto;
 
-   var diplayImg = toDisplay;
+   var diplayImg = document.getElementsByClassName("foto-to-display")[0];
    var captionText = capt;
    img.onclick = function(){
 
@@ -17,7 +17,7 @@ function enlarge(foto,display,toDisplay,capt) {
    }
    // Get the <span> element that closes the modal
    var xOut = document.getElementsByClassName("close")[0];
-   
+
    // When the user clicks on <span> (x), close the modal
    xOut.onclick = function() {
      dis.style.display = "none";
